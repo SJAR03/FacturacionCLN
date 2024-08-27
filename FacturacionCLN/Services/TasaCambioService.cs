627548f52fbc7302459101905e772c3405d0e834
Sergio Ayerdis <72574282+SJAR03@users.noreply.github.com>
2024-08-27 00:26:33 -0600

    Finished TasaCambio controller and service.
    Validation for value of tasa, must be a positive number.
    
    It can list the daily exchange rate and the exhange rate by month

---
+++ FacturacionCLN/Services/TasaCambioService.cs
@@ -0,0 +1,15 @@
+﻿namespace FacturacionCLN.Services
+{
+    public class TasaCambioService
+    {
+        public (bool IsValid, string ErrorMessage) ValidarTasaCambio(decimal tasaCambio)
+        {
+            if (tasaCambio <= 0)
+            {
+                return (false, "La tasa de cambio debe ser un número positivo.");
+            }
+
+            return (true, null);
+        }
+    }
+}
